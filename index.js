dfffbc318dad871fbb18eb2e2fe59fa729060b30
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2021-05-18 16:44:08 -0400

    Added click capability

--- index.js
+++ index.js
@@ -2,32 +2,56 @@
 let clapBtn = document.querySelector('.clap-btn')
 let clapAudio = document.getElementById('clap-audio')
 clapBtn.addEventListener('click', function (e) {
-
+    handleClick(clapBtn, clapAudio)
 })
 
 let hiHatBtn = document.querySelector('.hi-hat-btn')
 let hiHatAudio = document.getElementById('hi-hat-audio')
+hiHatBtn.addEventListener('click', function (e) {
+    handleClick(hiHatBtn, hiHatAudio)
+})
 
 let kickBtn = document.querySelector('.kick-btn')
 let kickAudio = document.getElementById('kick-audio')
+kickBtn.addEventListener('click', function (e) {
+    handleClick(kickBtn, kickAudio)
+})
 
 let openHatBtn = document.querySelector('.open-hat-btn')
 let openHatAudio = document.getElementById('open-hat-audio')
+openHatBtn.addEventListener('click', function (e) {
+    handleClick(openHatBtn, openHatAudio)
+})
 
 let boomBtn = document.querySelector('.boom-btn')
 let boomAudio = document.getElementById('boom-audio')
+boomBtn.addEventListener('click', function (e) {
+    handleClick(boomBtn, boomAudio)
+})
 
 let rideBtn = document.querySelector('.ride-btn')
 let rideAudio = document.getElementById('ride-audio')
+rideBtn.addEventListener('click', function (e) {
+    handleClick(rideBtn, rideAudio)
+})
 
 let snareBtn = document.querySelector('.snare-btn')
 let snareAudio = document.getElementById('snare-audio')
+snareBtn.addEventListener('click', function (e) {
+    handleClick(snareBtn, snareAudio)
+})
 
 let tomBtn = document.querySelector('.tom-btn')
 let tomAudio = document.getElementById('tom-audio')
+tomBtn.addEventListener('click', function (e) {
+    handleClick(tomBtn, tomAudio)
+})
 
 let tinkBtn = document.querySelector('.tink-btn')
 let tinkAudio = document.getElementById('tink-audio')
+tinkBtn.addEventListener('click', function (e) {
+    handleClick(tinkBtn, tinkAudio)
+})
 
 document.body.addEventListener('keypress', function (e) {
     let key = e.key.toLowerCase()
@@ -66,8 +90,6 @@ document.body.addEventListener('keypress', function (e) {
 
 
 })
-
-let hande
 
 let handleClick = (element, audio) => {
     element.classList.add('click')
